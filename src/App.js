1afb7a77d0f219a3798a483f79123fe8edf46dff
Kishan Kumar <kishan.kumar@zimtive.com>
2023-04-13 00:05:06 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Hero from './components/Hero/Hero';
-import Join from './components/Join/Join';
-import Plans from './components/Plans/Plans';
-import Programs from './components/Programs/Programs';
-import Reasons from './components/Reasons/Reasons';
-import Testimonials from './components/Testimonials/Testimonials';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import Main from './components/Main';
+import { Route, Routes} from 'react-router-dom';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import EmailVerify from './components/EmailVerify';
